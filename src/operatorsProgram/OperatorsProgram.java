eb376c1f37e339093a44fb6ccd17b5069bfa1908
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-07-27 11:22:24 +0530

    Operators d4

---
+++ src/operatorsProgram/OperatorsProgram.java
@@ -0,0 +1,22 @@
+package operatorsProgram;
+
+public class OperatorsProgram {
+
+    public static void main(String[] args) {
+
+        int a = 4, b = 6;
+
+        // Arithmetic Operators (+, -, *, /)
+
+        int sum = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        int div = a / b;
+
+        System.out.println("Hey");
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+    }
+}

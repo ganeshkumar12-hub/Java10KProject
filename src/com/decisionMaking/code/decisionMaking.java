b63a45e6026fe57f59cc82160ec48cb96effeacd
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-07-28 11:21:12 +0530

    decision making d5

---
+++ src/com/decisionMaking/code/decisionMaking.java
@@ -0,0 +1,17 @@
+package com.decisionMaking.code;
+
+public class decisionMaking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 18;
+		if(a>=18) {
+			System.out.println("eligible for voting");
+		}
+		else {
+			System.out.println("not eligible for voting	");
+		}
+
+	}
+
+}

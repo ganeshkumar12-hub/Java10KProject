1dec7914f09b4dcc7c33c8635f8d9ed1e1d3eff2
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-08-06 11:07:23 +0530

    scanner class

---
+++ src/com/coder/scanner/scannerExm.java
@@ -0,0 +1,14 @@
+package com.coder.scanner;
+import java.util.*;
+public class scannerExm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("enter the value of n: ");
+		int n = sc.nextInt();
+		System.out.println(n);
+		sc.close();
+	}
+
+}

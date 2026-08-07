b1eb2cb0f870a3fd60325f27a37d99d213042835
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-08-07 11:00:50 +0530

    methods

---
+++ src/com/coder/methods/methodex.java
@@ -0,0 +1,17 @@
+package com.coder.methods;
+
+public class methodex {
+	
+	static int add(int a, int b) {
+			return a+b;
+		}
+	static int add(int a , int b, int c) {
+		return a+b+c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(2,3));
+
+	}
+
+}

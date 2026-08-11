aadd0b0bb255c32f48e7d2f090ec07d54fff24aa
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-08-11 14:56:56 +0530

    task calculator using methods

---
+++ src/com/coder/myPrepJava/task41.java
@@ -0,0 +1,26 @@
+package com.coder.myPrepJava;
+
+public class task41 {
+	
+	static int add(int a, int b) {
+		return a + b;
+	}
+	
+	static int sub(int a, int b) {
+		return a - b;
+	}
+	static int multi(int a, int b) {
+		return a*b;
+	}
+	static int div(int a, int b) {
+		return a/b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("addition : "+add(3,4));
+		System.out.println("substraction : "+sub(10,6));
+		System.out.println("multiplication : "+multi(9,4));
+		System.out.println("division : "+div(5,2));
+	}
+
+}

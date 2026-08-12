244acda598d6941aadbf675939d388704f151ef1
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-08-12 11:04:33 +0530

    String and methods

---
+++ src/com/coder/strings/StringEx.java
@@ -0,0 +1,15 @@
+package com.coder.strings;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "hello world";
+		System.out.println("length    : "+s.length());
+		System.out.println("Upper     : "+s.toUpperCase());
+		System.out.println("lower     : "+s.toLowerCase());
+		System.out.println("Substring : "+s.substring(1,3));
+		System.out.println("char at   : "+s.charAt(0));
+	}
+
+}

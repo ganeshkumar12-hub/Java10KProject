2a509e5e3680106736ae93d1baee0df034695b1c
Seeripi Ganesh Kumar <seeripi.ganeshkumar@gmail.com>
2026-08-13 16:03:49 +0530

    task 21 done

---
+++ src/com/coder/myPrepJava/task21.java
@@ -0,0 +1,24 @@
+package com.coder.myPrepJava;
+import  java.util.*;
+
+import java.util.Scanner;
+
+public class task21 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println("---------------------------------");
+        System.out.println("       STUDENT ATTENDANCE");
+        System.out.println("---------------------------------");
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter Student " + i + " name: ");
+            String name = sc.nextLine();
+            System.out.println("Student " + i + " : " + name);
+        }
+    }
+}
